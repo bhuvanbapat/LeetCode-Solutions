cff979be92023956eb1c998b971ef5097eb23704
Bhuvan Bapat <bhuvanbapat@gmail.com>
2026-09-17 06:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.11%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,19 +1,20 @@
 class Solution {
     public int tribonacci(int n) {
-        int dp[]=new int[n+1];
-        if(n==0){
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        if (n == 0) {
             return 0;
-        }
-        else if(n==1 || n==2 ){
+        } else if (n == 1 || n == 2) {
             return 1;
+        } else {
+            for (int i = 3; i <= n; i++) {
+                int d = a + b + c;
+                a = b;
+                b = c;
+                c = d;
+            }
         }
-        else{
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        }}
-        return dp[n];
+        return c;
     }
 }
